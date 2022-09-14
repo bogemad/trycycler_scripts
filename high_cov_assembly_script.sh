2927a90076d89b08f37c1fb29d33d4a32a31da4c
Daniel Bogema <daniel.bogema@dpi.nsw.gov.au>
2022-09-14 15:41:04 +1000

    added nanohq option to flye

--- high_cov_assembly_script.sh
+++ high_cov_assembly_script.sh
@@ -3,7 +3,7 @@
 #run as bash high_cov_assembly_script.sh <sample_name> <reads> <genome_size> <threads> <out_dir>
 
 sample_name=$1
-target_depth=50
+target_depth=100
 reads=$2
 genome_size=$3
 threads=$4
@@ -23,7 +23,6 @@ mkdir -p $out_dir/assemblies
 
 mkdir -p $logs
 
-
 for ass in flye miniasm raven redbean necat shasta; do
 	if [[ $ass == "flye" ]]; then #Assemble w/ Flye
 		for i in {1..3}; do
@@ -33,8 +32,15 @@ for ass in flye miniasm raven redbean necat shasta; do
 				sreads=$workdir/read_sample."$i".fastq
 				seqtk sample -s "$i" $freads "$read_count" | paste - - - - | shuf | tr '\t' '\n' > $sreads
 				echo "Running $ass assembly $i"
-				flye --nano-raw $sreads --genome-size "$genome_size" --threads "$threads" --plasmids --out-dir $workdir/assembly &> $logs/$ass.$i.log.txt && echo "$ass assembly $i successful" || echo "$ass assembly $i failed" 
-				cp $workdir/assembly/assembly.fasta $out_dir/assemblies/$sample_name.$ass."$i".fasta
+				flye --nano-hq $sreads -g "$genome_size" --threads "$threads" -o $workdir/assembly &> $logs/$ass.$i.log.txt
+				RESULT=$?
+				if [ $RESULT -eq 0 ]; then
+					echo "$ass assembly $i successful"
+					cp $workdir/assembly/assembly.fasta $out_dir/assemblies/$sample_name.$ass."$i".fasta
+					assembly-stats $out_dir/assemblies/$sample_name.$ass."$i".fasta | grep -E 'sum|N50'
+				else
+					echo "$ass assembly $i failed"
+				fi
 				rm -r $workdir
 			fi
 		done
@@ -46,8 +52,15 @@ for ass in flye miniasm raven redbean necat shasta; do
 				sreads=$workdir/read_sample."$i".fastq
 				echo "Running $ass assembly $i"
 				seqtk sample -s "$i" $freads "$read_count" | paste - - - - | shuf | tr '\t' '\n' > $sreads
-				miniasm_and_minipolish.sh $sreads "$threads" 1> $workdir/assembly.gfa 2> $logs/$ass.$i.log.txt && echo "$ass assembly $i successful" || echo "$ass assembly $i failed" 
-				any2fasta $workdir/assembly.gfa > $out_dir/assemblies/$sample_name.$ass."$i".fasta
+				miniasm_and_minipolish.sh $sreads "$threads" 1> $workdir/assembly.gfa 2> $logs/$ass.$i.log.txt
+				RESULT=$?
+				if [ $RESULT -eq 0 ]; then
+					echo "$ass assembly $i successful"
+					any2fasta $workdir/assembly.gfa 1> $out_dir/assemblies/$sample_name.$ass."$i".fasta 2> $logs/$ass.$i.log.txt
+					assembly-stats $out_dir/assemblies/$sample_name.$ass."$i".fasta | grep -E 'sum|N50'
+				else
+					echo "$ass assembly $i failed"
+				fi
 				rm -r $workdir
 			fi
 		done
@@ -60,7 +73,14 @@ for ass in flye miniasm raven redbean necat shasta; do
 				echo "Running $ass assembly $i"
 				seqtk sample -s "$i" $freads "$read_count" | paste - - - - | shuf | tr '\t' '\n' > $sreads
 				cd $workdir
-				raven --threads "$threads" $(basename $sreads) 1> $out_dir/assemblies/$sample_name.$ass."$i".fasta 2> $logs/$ass.$i.log.txt && echo "$ass assembly $i successful" || echo "$ass assembly $i failed" 
+				raven --threads "$threads" $(basename $sreads) 1> $out_dir/assemblies/$sample_name.$ass."$i".fasta 2> $logs/$ass.$i.log.txt
+				RESULT=$?
+				if [ $RESULT -eq 0 ]; then
+					echo "$ass assembly $i successful"
+					assembly-stats $out_dir/assemblies/$sample_name.$ass."$i".fasta | grep -E 'sum|N50'
+				else
+					echo "$ass assembly $i failed"
+				fi
 				cd -
 				rm -r $workdir
 			fi
@@ -73,9 +93,26 @@ for ass in flye miniasm raven redbean necat shasta; do
 				sreads=$workdir/read_sample."$i".fastq
 				echo "Running $ass assembly $i"
 				seqtk sample -s "$i" $freads "$read_count" | paste - - - - | shuf | tr '\t' '\n' > $sreads
-				wtdbg2 -o $workdir/assembly -g "$genome_size" -t "$threads" -x ont -i $sreads &> $logs/$ass.$i.log.txt && echo "$ass assembly $i successful" || echo "$ass assembly $i failed" 
-				wtpoa-cns -t "$threads" -i $workdir/assembly.ctg.lay.gz -o $workdir/assembly.ctg.fasta &> $logs/$ass.$i.cns.log.txt && echo "$ass cns assembly $i successful" || echo "$ass cns assembly $i failed"
-				mv $workdir/assembly.ctg.fasta $out_dir/assemblies/$sample_name.$ass."$i".fasta
+				wtdbg2 -o $workdir/assembly -g "$genome_size" -t "$threads" -x ont -i $sreads &> $logs/$ass.$i.log.txt
+				RESULT=$?
+				if [ $RESULT -eq 0 ]; then
+					echo "$ass assembly $i successful"
+					wtpoa-cns -t "$threads" -i $workdir/assembly.ctg.lay.gz -fo $workdir/assembly.cns.fa &>> $logs/$ass.$i.log.txt
+				else
+					echo "$ass assembly $i failed"
+					rm -r $workdir
+					continue
+				fi	
+				RESULT2=$?
+				if [ $RESULT2 -eq 0 ]; then
+					echo "$ass consensus $i successful"
+					mv $workdir/assembly.cns.fa $out_dir/assemblies/$sample_name.$ass."$i".fasta
+					assembly-stats $out_dir/assemblies/$sample_name.$ass."$i".fasta | grep -E 'sum|N50'
+				else
+					echo "$ass consensus $i failed" 
+					rm -r $workdir
+					continue
+				fi	
 				rm -r $workdir
 			fi
 		done
@@ -113,11 +150,39 @@ FSA_CTG_BRIDGE_OPTIONS=
 POLISH_CONTIGS=true
 " > $workdir/assembly.config
 				cd $workdir/
-				necat.pl correct $workdir/assembly.config &> $logs/$ass.$i.correct.log.txt && echo "$ass correct $i successful" || echo "$ass correct $i failed" 
-				necat.pl assemble $workdir/assembly.config &> $logs/$ass.$i.assemble.log.txt && echo "$ass assemble $i successful" || echo "$ass assemble $i failed" 
-				necat.pl bridge $workdir/assembly.config &> $logs/$ass.$i.bridge.log.txt && echo "$ass bridge $i successful" || echo "$ass bridge $i failed" 
-				cd -
-				mv $workdir/assembly/6-bridge_contigs/polished_contigs.fasta $out_dir/assemblies/$sample_name.$ass."$i".fasta
+				necat correct $workdir/assembly.config &> $logs/$ass.$i.correct.log.txt
+				RESULT=$?
+				if [ $RESULT -eq 0 ]; then
+					echo "$ass correct $i successful"
+					necat assemble $workdir/assembly.config &> $logs/$ass.$i.assemble.log.txt
+				else
+					echo "$ass correct $i failed"
+					cd - &> /dev/null
+					rm -r $workdir
+					continue
+				fi
+				RESULT=$?
+				if [ $RESULT -eq 0 ]; then
+					echo "$ass assemble $i successful"
+					necat bridge $workdir/assembly.config &> $logs/$ass.$i.bridge.log.txt
+				else
+					echo "$ass assemble $i failed"
+					cd - &> /dev/null
+					rm -r $workdir
+					continue
+				fi
+				RESULT=$?
+				if [ $RESULT -eq 0 ]; then
+					echo "$ass bridge $i successful"
+					mv $workdir/assembly/6-bridge_contigs/polished_contigs.fasta $out_dir/assemblies/$sample_name.$ass."$i".fasta
+					assembly-stats $out_dir/assemblies/$sample_name.$ass."$i".fasta | grep -E 'sum|N50'
+				else
+					echo "$ass bridge $i failed"
+					cd - &> /dev/null
+					rm -r $workdir
+					continue
+				fi
+				cd - &> /dev/null
 				rm -r $workdir
 			fi
 		done
@@ -129,8 +194,15 @@ POLISH_CONTIGS=true
 				sreads=$workdir/read_sample."$i".fastq
 				echo "Running $ass assembly $i"
 				seqtk sample -s "$i" $freads "$read_count" | paste - - - - | shuf | tr '\t' '\n' > $sreads
-				shasta --input $sreads --config Nanopore-Oct2021 --assemblyDirectory $workdir/assembly --threads $threads --Reads.minReadLength 1000 &> $logs/$ass.$i.log.txt && echo "$ass assembly $i successful" || echo "$ass assembly $i failed" 
-				mv $workdir/assembly/Assembly.fasta $out_dir/assemblies/$sample_name.$ass."$i".fasta
+				shasta --input $sreads --config Nanopore-May2022 --assemblyDirectory $workdir/assembly --threads $threads --Reads.minReadLength 1000 &> $logs/$ass.$i.log.txt
+				RESULT=$?
+				if [ $RESULT -eq 0 ]; then
+					echo "$ass assembly $i successful"
+					mv $workdir/assembly/Assembly.fasta $out_dir/assemblies/$sample_name.$ass."$i".fasta
+					assembly-stats $out_dir/assemblies/$sample_name.$ass."$i".fasta | grep -E 'sum|N50'
+				else
+					echo "$ass assembly $i failed"
+				fi
 				rm -r $workdir
 			fi
 		done
@@ -140,6 +212,7 @@ POLISH_CONTIGS=true
 	fi
 done
 
+echo "Running trycycler cluster..."
+trycycler cluster --assemblies $out_dir/assemblies/$sample_name.*.fasta --reads $freads --out_dir $out_dir/$sample_name.trycycler --threads $threads &> $out_dir/$sample_name.trycycler.log.txt
 
-trycycler cluster --assemblies $out_dir/assemblies/$sample_name.*.fasta --reads $freads --out_dir $sample_name.trycycler --threads $threads
-
+echo "$sample_name assembly and clustering complete."
